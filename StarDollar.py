7b8d17b3ce6e99c4656987d3bcca26cd25914612
PuvaneshwaranV <vpuvaneshwaran5@gmail.com>
2024-09-21 19:26:54 +0530

    commit -m "Modified StarDollar"

--- StarDollar.py
+++ StarDollar.py
@@ -1,16 +1,23 @@
-n=5
+n=input("Enter the Value")
 i=0
 j=0
 p=""
 i=0
-while(i<n):
-    j=0
-    while(j<n):
-        if(i==j):
-            p+="*"
-        else:
-            p+="$"
-        j=j+1
-    p+="\n"
-    i=i+1
+if(n.isdigit() or n[0]=='-' and n[1:].isdigit()):
+    n=int(n)
+    if(n>0):
+        while(i<n):
+            j=0
+            while(j<n):
+                if(i==j):
+                    p+="*"
+                else:
+                    p+="$"
+                j=j+1
+            p+="\n"
+            i=i+1
+    else:
+        print("Value must be greater than 0")
+else:
+     print("Please enter the digit only")
 print(p)
